bc180532a147d902210ded7d9ff2b76b53755617
vitorbizarra <vitorbizarracontato@gmail.com>
2023-05-18 00:08:54 -0300

    :heavy_plus_sign: build: Adding JPA dependency

--- src/main/java/com/mycompany/dejep/Dejep.java
+++ src/main/java/com/mycompany/dejep/Dejep.java
@@ -2,8 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.dejep;
+
+import com.mycompany.dejep.models.Book;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
@@ -13,5 +17,15 @@ public class Dejep {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Dejep-PU");
+        EntityManager entityManager = emf.createEntityManager();
+
+        Book book = entityManager.find(Book.class, 1);
+
+        System.out.println("\n" + book.getName() + "\n");
+
+        entityManager.close();
+        emf.close();
     }
 }
---
+++ src/main/java/com/mycompany/dejep/models/Book.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dejep.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author titib
+ */
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
