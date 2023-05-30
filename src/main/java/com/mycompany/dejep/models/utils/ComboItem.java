15b3246660cce97741f55f8128d8c93244045904
vitorbizarra <vitorbizarracontato@gmail.com>
2023-05-29 22:50:38 -0300

    :wrench: chore: Adding ComboItem class

---
+++ src/main/java/com/mycompany/dejep/models/utils/ComboItem.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dejep.models.utils;
+
+/**
+ *
+ * @author titib
+ */
+public class ComboItem {
+
+    private String key;
+    private String value;
+
+    public ComboItem(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return key;
+    }
+}
